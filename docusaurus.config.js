a92752566345938b3a44eb6cbdf756caba635e21
febriadj <iamfebriadji@gmail.com>
2023-03-08 15:57:11 +0700

    refactor: format project with prettier

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,51 +6,52 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: "Discuzz",
-  tagline: "Instant messaging app that allows you to quickly send and receive text messages, emojis, photos, and audio with other Discuzz users",
-  favicon: "img/favicon.ico",
+  title: 'Discuzz',
+  tagline:
+    'Instant messaging app that allows you to quickly send and receive text messages, emojis, photos, and audio with other Discuzz users',
+  favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: "https://your-docusaurus-test-site.com",
+  url: 'https://your-docusaurus-test-site.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/",
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: "febriadj", // Usually your GitHub org/user name.
-  projectName: "discuzz-docs", // Usually your repo name.
+  organizationName: 'febriadj', // Usually your GitHub org/user name.
+  projectName: 'discuzz-docs', // Usually your repo name.
 
-  onBrokenLinks: "throw",
-  onBrokenMarkdownLinks: "warn",
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
   // to replace "en" with "zh-Hans".
   i18n: {
-    defaultLocale: "en",
-    locales: ["en"],
+    defaultLocale: 'en',
+    locales: ['en'],
   },
 
   presets: [
     [
-      "classic",
+      'classic',
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
         docs: {
-          sidebarPath: require.resolve("./sidebars.js"),
+          sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl: "https://github.com/febriadj/discuzz-docs/tree/master",
+          editUrl: 'https://github.com/febriadj/discuzz-docs/tree/master',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl: "https://github.com/febriadj/discuzz-docs/tree/master",
+          editUrl: 'https://github.com/febriadj/discuzz-docs/tree/master',
         },
         theme: {
-          customCss: require.resolve("./src/css/custom.css"),
+          customCss: require.resolve('./src/css/custom.css'),
         },
       }),
     ],
@@ -60,66 +61,66 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       // Replace with your project's social card
-      image: "img/docusaurus-social-card.jpg",
+      image: 'img/docusaurus-social-card.jpg',
       navbar: {
-        title: "Discuzz",
+        title: 'Discuzz',
         logo: {
-          alt: "Discuzz Logo",
-          src: "img/logo.svg",
+          alt: 'Discuzz Logo',
+          src: 'img/logo.svg',
         },
         items: [
           {
-            type: "doc",
-            docId: "intro",
-            position: "left",
-            label: "Docs",
+            type: 'doc',
+            docId: 'intro',
+            position: 'left',
+            label: 'Docs',
           },
           {
-            to: "/blog",
-            label: "Blog",
-            position: "left",
+            to: '/blog',
+            label: 'Blog',
+            position: 'left',
           },
           {
-            href: "https://github.com/febriadj/discuzz-docs",
-            label: "GitHub",
-            position: "right",
+            href: 'https://github.com/febriadj/discuzz-docs',
+            label: 'GitHub',
+            position: 'right',
           },
         ],
       },
       footer: {
         links: [
           {
-            title: "Docs",
+            title: 'Docs',
             items: [
               {
-                label: "Introduction",
-                to: "/docs/intro",
+                label: 'Introduction',
+                to: '/docs/intro',
               },
             ],
           },
           {
-            title: "Support",
+            title: 'Support',
             items: [
               {
-                label: "Github Discussions",
-                href: "https://github.com/febriadj/discuzz-docs/discussions",
+                label: 'Github Discussions',
+                href: 'https://github.com/febriadj/discuzz-docs/discussions',
               },
               {
-                label: "Help",
-                href: "mailto:help.discuzz@gmail.com",
+                label: 'Help',
+                href: 'mailto:help.discuzz@gmail.com',
               },
             ],
           },
           {
-            title: "More",
+            title: 'More',
             items: [
               {
-                label: "Github",
-                href: "https://github.com/febriadj/discuzz-docs",
+                label: 'Github',
+                href: 'https://github.com/febriadj/discuzz-docs',
               },
               {
-                label: "Report Issue",
-                href: "https://github.com/febriadj/discuzz-docs/issues/new",
+                label: 'Report Issue',
+                href: 'https://github.com/febriadj/discuzz-docs/issues/new',
               },
             ],
           },
